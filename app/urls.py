9711ad9ddb976141502e4046e460d25a2bcbca31
Krish123-lang <mandalkrishna739@gmail.com>
2023-10-30 13:01:26 +0545

    Creating Header, Footer, SideBar, Index and Base Template files

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views, Hod_Views, Staff_Views, Student_Views
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    path('login', views.login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'app/index.html')
+
+
+def login(request):
+    return render(request, 'app/login.html')
